50365a65a26647c15555635108b3300407885870
Thomson Situmorang <2381049@unai.edu>
2025-02-18 11:19:53 +0700

    First Commit

---
+++ Handson 5/src/Components/ContactInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactInfo = () => {
+  return (
+    <div>
+        <p>Email: example@gmail.com</p>
+        <p>Phone: 085158424233</p>
+        <p>Address: xyz street, indonesia</p>
+    </div>
+  )
+}
+
+export default ContactInfo
--- Handson 5/src/Components/Navbar.tsx
+++ Handson 5/src/Components/Navbar.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { NavLink}
-import logo from "../assets/Logo.png";
-function Navbar() {
+import React from 'react'
+import { Navigate, NavLink, useNavigate } from 'react-router-dom'
+
+
+const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div className='navbar'>
-      <img src={logo} alt="" width={180} height={30}/>
-      <ul>
-        <NavLink tp="/">
-        <li>Home</li>
-        </NavLink>
-        <NavLink tp="/product">
-        <li>Product</li>
-        </NavLink>
-        <NavLink tp="/about">
-        <li>About</li>
-        </NavLink>
-        <NavLink tp="/contact">
-        <li>Contact</li>
-        </NavLink>
-        <NavLink tp="/jobs">
-        <li>Jobs</li>
-        </NavLink>
-      </ul>
+        <img src="/Logo.png" alt="react logo " width={180} height={30}/>
+        <ul>
+            <NavLink to="/">
+                <li>Home</li>
+            </NavLink>
+            <NavLink to="/product">
+                <li>Product</li>
+            </NavLink> 
+            <NavLink to="/about">
+                <li>About</li>
+            </NavLink> 
+            <NavLink to="/contact">
+                <li>Contact</li>
+            </NavLink> 
+            <NavLink to="/jobs">
+                <li>Jobs</li>
+            </NavLink>
+        </ul>
+        <button onClick={() => navigate("/about")}>Get Started</button>
     </div>
   )
 }
